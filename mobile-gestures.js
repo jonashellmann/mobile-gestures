fee5881b80b63934d74b812154d3f1810f7425d6
Jonas Hellmann <mail@jonas-hellmann.de>
2017-12-18 10:40:44 +0100

    Add source code of version 0.1.0 to the repository

---
+++ mobile-gestures.js
@@ -0,0 +1,29 @@
+var myElement = $( "body" );
+var mc = new Hammer.Manager(myElement[0]);
+
+mc.add( new Hammer.Tap( { event: 'tripletap', taps: 3 } ) );
+mc.add( new Hammer.Swipe( { event: 'triple-swipe-down', pointers: 3, direction: Hammer.DIRECTION_DOWN } ) );
+mc.add( new Hammer.Swipe( { event: 'triple-swipe-up', pointers: 3, direction: Hammer.DIRECTION_UP } ) );
+
+function reloadCurrentTab() {
+	location.reload();
+}
+
+function openNewTab() {
+	var w = window.open("about:blank", "_blank");
+	w.focus();
+}
+
+mc.on("tripletap triple-swipe-down", function(ev) {
+    openNewTab();
+});
+
+mc.on("triple-swipe-up", function(ev) {
+    reloadCurrentTab();
+});
+
+
+// https://developer.mozilla.org/en-US/Add-ons/WebExtensions/manifest.json/permissions --> Berechtigungen in manifest.json
+// https://developer.mozilla.org/en-US/Add-ons/WebExtensions/manifest.json/browser_action --> Toolbar
+// https://developer.mozilla.org/en-US/Add-ons/WebExtensions/Examples --> Beispiele
+// https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/tabs --> Tabs
